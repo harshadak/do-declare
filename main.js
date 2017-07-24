a0a750d7640198361ead553ef0a8c6625198330b
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-07-24 13:11:34 -0700

    Added the variables and printed their values

---
+++ main.js
@@ -0,0 +1,26 @@
+var num1 = 10,
+    num2 = -1,
+    num3 = 1.25,
+    num4 = 20;
+var string1 = "Hi Ninjas! ",
+    string2 = "Happy Coding ",
+    string3 = "at Coding Dojo!";
+var bool1 = true,
+    bool2 = false;
+var undefinedVar;
+
+// Printing the numbers
+console.log("Number 1 = " + num1);
+console.log("Number 2 = " + num2);
+console.log("Number 3 = " + num3);
+console.log("Number 4 = " + num4);
+
+// Printing the strings
+console.log("This greeting is for you - " + string1 + string2 + string3);
+
+// Printing boolean values
+console.log("bool1 = " + bool1);
+console.log("bool2 = " + bool2);
+
+// Printing undefined variable
+console.log(undefinedVar);
